453a6c88d769885d2a65144c283be5489e59fe9b
dan <daniel_tzonkov@abv.bg>
2023-09-05 13:08:01 +0300

    UP

--- src/main/java/entities/Student.java
+++ src/main/java/entities/Student.java
@@ -1,4 +1,35 @@
 package entities;
 
 public class Student {
+    private int id;
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
+
